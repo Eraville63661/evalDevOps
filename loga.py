ef06b9e711216489be9eb30949d1d7135f5d8d70
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2024-01-16 11:24:17 +0100

    workflows

--- loga.py
+++ loga.py
@@ -1,10 +1,10 @@
 def loga(a, b):
-    import log from numpy
-    # c = np.log(a*b) #ils vont etre égaux ?
-    # d = np.log(a) + np.log(b)
+    import numpy as np
+    c = np.log(a*b) #ils vont etre égaux ?
+    d = np.log(a) + np.log(b)
 
-    c = log(a*b) #ils vont etre égaux ?
-    d = log(a) + log(b)
+    # c = log(a*b) #ils vont etre égaux ?
+    # d = log(a) + log(b)
 
     print(c)
     print(d)
